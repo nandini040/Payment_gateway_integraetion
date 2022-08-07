23dcef02e0bfb4d92a562893b28ad56e4be8d4e5
Nandini <68437129+nandini040@users.noreply.github.com>
2022-08-07 15:44:52 +0530

    Removed unwanted lines

--- app.js
+++ app.js
@@ -2,22 +2,6 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
-const modalBtns = document.querySelectorAll('.modal-open');
-
-modalBtns.forEach(function(btn){
-	btn.onclick = function(){
-		var modal = btn.getAttribute("data-modal");
-		document.getElementById(modal).style.display = "block";
-	};
-});
-
-const closeBtns = document.querySelectorAll(".modal-close");
-
-closeBtns.forEach(function(btn){
-btn.onclick = function(){
-	var modal = (btn.closest(".modal").style.display="none");
-}
-});
 
 window.onclick = function(e){
 	if(e.target.className == "modal"){
